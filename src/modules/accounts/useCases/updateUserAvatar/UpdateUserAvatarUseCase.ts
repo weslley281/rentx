450cb3f7dee30446d3cac099520f31d4782853b0
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-08-21 23:07:06 -0400

    first step completed

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,5 +1,6 @@
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { inject, injectable } from "tsyringe";
+import { deleteFile } from "../../../../utils/file";
 
 interface IRequest {
   user_id: string;
@@ -16,7 +17,10 @@ class UpdateUserAvatarUseCase {
   async execute({ user_id, avatar_file }: IRequest): Promise<void> {
     const user = await this.userRepository.findById(user_id);
 
-    user.avatar = avatar_file;
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
+      user.avatar = avatar_file;
+    }
 
     await this.userRepository.create(user);
   }
